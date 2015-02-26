29a94388a055507e5208dd29d51c45f2c5ea3d8d
Jiawei Chen <jchen2120212@gmail.com>
2015-02-25 22:04:41 -0500

    Removed BallGroup--deemed as redundant and unnecessary for our use. May break code. Actually, will DEFINITELY break code that uses the BallGroup class. Sorry!

--- goals.py
+++ goals.py
@@ -21,7 +21,7 @@ YELLOW = (255, 189, 0)
 GREEN = (0, 168, 0)
 
 colorlist = [RED, YELLOW, GREEN, BLUE] #List of colors for balls--the RNG will select a random color to make the next ball.
-
+attrlist = ["RED", "YELLOW", "GREEN", "BLUE"]
 myLog = gamelog.Logger(SCREEN)
 myLog.maxlines = 10
 #End Setup Thingies##############
@@ -39,6 +39,7 @@ class Ball:
         self.blurlist = []
         self.surf = surf
         self.radius = radius
+        self.goalcollide = False #Is ball colliding with goal zone?
 
     def update(self): #Updates the ball's position, velocity, and is where the physics for the ball happen.
         self.vel = [self.vel[0] + self.acc[0], self.vel[1] + self.acc[1]]
@@ -47,7 +48,7 @@ class Ball:
                        self.coords[1] + self.vel[1]]
         
         self.blurlist = []
-        
+        self.goalcollide = self.coords[1] <= 25 + self.radius
     def bounceY(self):
         self.vel[1] = -self.vel[1]- self.acc[1]
         
@@ -63,41 +64,6 @@ class Ball:
         pygame.gfxdraw.filled_circle(self.surf, int(self.coords[0]),                                      int(self.coords[1]), self.radius, self.color)
         #Circles are kinda like chocolate truffles--smooth on the outside, filled on the inside...
         
-class BallGroup:
-    def __init__(self):
-        self.objects = []
-        self.size = 0 #Keeps track of how many balls are in list
-    def add(self, instance):
-        self.objects.append(instance)
-    def draw(self):
-        for items in self.objects:
-            items.draw()
-    def remove(self, obj):#Because "kill" sounds too morbid...
-        del(obj)
-        
-    def update(self):
-        count = 0
-        
-        while count < len(self.objects):#Allows the list to become modifiable due to advanced for loops being immutable                            
-            items = self.objects[count]
-            items.update()
-            if (items.coords[0] >= resX - items.radius or 
-                    items.coords[0] <= items.radius):
-                items.bounceX()
-                
-            if items.coords[1] > resY + 200:
-                #items.bounceY()
-                del(self.objects[count])
- 
-            count += 1
-            
-        self.size = len(self.objects)
-        
-   
-        
-    def clear(self): #Deletes all objects in group
-        for num in range(0, len(self.objects)):
-            del(self.objects[-1])
 
 #Goal Code Start########
 BLUEGOAL = range(0, int(resX / 4) - 1)
@@ -105,20 +71,36 @@ GREENGOAL = range(int(resX / 4), int(resX / 2) - 1)
 YELLOWGOAL = range(int(resX / 2), int(resX * 3 / 4) - 1)
 REDGOAL = range(int(resX * 3 / 4), int(resX))
 GOALHEIGHT = 25
+goallist = [REDGOAL, YELLOWGOAL, GREENGOAL, BLUEGOAL]
 #Goal Code End########
 
-myGroup = BallGroup()
+#Other bits of initialization code
+ballcolor = 0
 mouse = (0, 0)
 myBall = Ball(SCREEN, RED, 
                     mouse, 10, 
                     (0, 0), (0, 0))
 myGroup.add(myBall)
+backcolor = WHITE
+#End init. code
 while True:
-    SCREEN.fill(WHITE) #Blanks screen
-    
+    SCREEN.fill(WHITE)
+   
     myBall.coords = mouse
-    
-    myGroup.draw()
+    myBall.color = colorlist[ballcolor]
+    myBall.property = attrlist[ballcolor]
+    if pygame.mouse.get_pressed() == (1, 0, 0):
+        myGroup.add(Ball(SCREEN, colorlist[ballcolor], mouse, 10, (0, -10), (0, 0), attrlist[ballcolor]))
+    myLog.log(len(myGroup.objects))
+    for items in myGroup.objects:
+        items.update()
+        items.draw()
+        attr = items.property
+        if items.goalcollide:
+            print("COLLIDE")
+            if items.coords[0] in goallist[attrlist.index(attr)]:
+                print("CORRECT!")
+            myGroup.remove(items)
     for num in range(0, 4):
         pygame.draw.rect(SCREEN, colorlist[3 - num],
                                  ((resX / 4) * num, 0, (resX / 4), GOALHEIGHT))
@@ -134,7 +116,15 @@ while True:
                 if keys[K_q] or keys[K_w]:
                     pygame.quit()
                     sys.exit()
-            
+            if keys[K_UP]:
+                ballcolor += 1
+                if ballcolor >= 4:
+                    ballcolor = 0
+            if keys[K_DOWN]:
+                ballcolor -= 1
+                if ballcolor < 0:
+                    ballcolor = 3
+                    
         if event.type == MOUSEMOTION:
             mouse = pygame.mouse.get_pos()
 
